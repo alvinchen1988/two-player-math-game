3741394d89cdc2d87e3f8356383d3aff615f4d02
Alvin Chen <alvinchen1988@icloud.com>
2016-03-04 16:23:35 -0800

    add game class

--- game.rb
+++ game.rb
@@ -1,77 +1,66 @@
-require 'pry'
-@player1_lives = 3
-@player2_lives = 3
+class Game
+  attr_accessor :on, :players, :winner
 
-game_begin = true
-
-print "Player 1, What is your name? "
-player1_name = gets.chomp
-print "Player 2, What is your name? "
-player2_name = gets.chomp
-while(game_begin)
-  number1 = rand(1..20)
-  number2 = rand(1..20)
-  math_signs = [:+, :-, :*, :/]
-  randomized_math_sign = math_signs.sample.to_s
-  # binding.pry
-  correct_answer = number1.send(randomized_math_sign, number2)
-  print "#{player1_name}, What does #{number1} #{randomized_math_sign} #{number2} equal?"
-  user_answer = gets.chomp.to_i
-  if user_answer != correct_answer
-    @player1_lives -= 1
-    puts "Incorrect Answer"
-    puts "#{player1_name} :#{@player1_lives}"
-    puts "#{player2_name}: #{@player2_lives}"
+  def initialize(player1, player2)
+    @players = [player1, player2]
+    @on = true
   end
 
-  if @player1_lives == 0
-    puts "#{player1_name}, You lost! #{player2_name} won by #{@player2_lives} points"
-    print "Do you want to play Again? Enter yes or no "
-    user_response = gets.chomp.downcase
-    if user_response == "yes"
-      @player1_lives = 3
-      @player2_lives = 3
-      redo
-    else
-      game_begin = false
-      break
+  def take_inputs_from_user (question)
+    puts question
+    return gets.chomp
+  end
+
+  def set_players_name
+    self.players.each do |player|
+      name = take_inputs_from_user("#{player.name}, What is your name?")
+      player.name = name
     end
   end
 
-  number1 = rand(1..20)
-  number2 = rand(1..20)
-  math_signs = [:+, :-, :*, :/]
-  randomized_math_sign = math_signs.sample
- 
-  correct_answer = number1.send(randomized_math_sign, number2)
-  print "#{player2_name}, What does #{number1} #{randomized_math_sign} #{number2} equal?"
-  user_answer = gets.chomp.to_i
+  def randomized_question
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    math_signs = [:+, :-, :*, :/]
+    randomized_math_sign = math_signs.sample
+    correct_answer = num1.send(randomized_math_sign, num2).to_s
 
-  if user_answer != correct_answer
-    @player2_lives -= 1
-    puts "Incorrect Answer"
-    puts "#{player1_name} :#{@player1_lives}"
-    puts "#{player2_name}: #{@player2_lives}"
+    question = {
+      num1: num1,
+      num2: num2,
+      math_sign: randomized_math_sign,
+      correct_answer: correct_answer
+    }
   end
 
-  if @player2_lives == 0
-    puts "#{player2_name}, You lost! #{player1_name} won by #{@player1_lives} points"
-    print "Do you want to play Again? Enter yes or no "
-    user_response = gets.chomp.downcase
-    if user_response == "yes"
-      @player1_lives = 3
-      @player2_lives = 3
-      redo
-    else
-      game_begin = false
-      break
+  def ask_and_check_answer
+    self.players.each do |player|
+      ques = randomized_question
+      player_answer = take_inputs_from_user("#{player.name}, what is #{ques[:num1]} #{ques[:math_sign]} #{ques[:num2]}")
+      if player_answer != ques[:correct_answer]
+        player.score -= 1
+        puts "#{player.name}, incorrect answer. Lost 1 point. #{player.score} points left"
+      else
+        puts "#{player.name}, You're right! #{player.score} points left"
+      end
+      game_over(player)
+    end
+
+  end
+
+  def game_over(player)
+    if player.score == 0
+      self.on = false
+      result = "#{player.name}, you lost the game! "
+      puts result
+
     end
   end
+  # takes winner and gives back result string   
+  # def game_over(winner)
+  #   # returns string of what to tell players
 
-  # if @player2_lives == 0
-  #   puts "#{player2_name}, You lost! #{player1_name} won by #{@player1_lives} points"
-  #   game_begin = false
-  #   break
+  #   result =  "#{player.name}, you lost the game!"     ###########################
+  #   result += "Want a new game? enter yes to restart the game"
   # end
-
-end 
+end
--- main.rb
+++ main.rb
@@ -1,38 +1,40 @@
 require 'pry'
+require './Game.rb'
 require './Player.rb'
-on_game = true
-player1 = Player.new("Player1")
-player2 = Player.new("Player2")
-players = [player1, player2]
+game = Game.new(Player.new("Player1"), Player.new("Player2"))
 
-  players.each do |player|
-    print "Hello , #{player.name}, what is your name ?"
-    player_name = gets.chomp
-    player.customize_name(player_name)
-  end
 
-  while (on_game)
+game.set_players_name
 
-      players.each do |player|
+while (game.on)
+  game.ask_and_check_answer
+end
 
-        num1 = rand(1..20)
-        num2 = rand(1..20)
-        math_signs = [:+, :-, :*, :/]
-        randomized_math_sign = math_signs.sample
-        puts "#{player.name}, what is #{num1} #{randomized_math_sign} #{num2}"
-        player_answer = gets.chomp
-        # binding.pry
-        player.check_answer(num1, num2, randomized_math_sign, player_answer)
-        # player.check
-        if player.is_dead? 
-          puts "#{player.name}, you lost the game!"     ###########################
-          puts "Want a new game? enter yes to restart the game"
-          player_answer = gets.chomp
-          on_game = false if player_answer != "yes"
-        end
+# game.players.each do |player|
 
-      end
+# end
+  
+  # while (game.on)
 
-  end
+  #     game.players.each do |player|
+
+
+
+  #       # player_answer = gets.chomp
+  #       # binding.pry
+  #       # player.check_answer(num1, num2, randomized_math_sign, player_answer)
+  #       # player.check
+  #       # if player.is_dead?
+
+  #         # puts "#{player.name}, you lost the game!"     ###########################
+  #         # puts "Want a new game? enter yes to restart the game"
+  #         # player_answer = gets.chomp
+
+  #         # on_game = false if player_answer != "yes" # ongame should be part of game instance
+  #       # end
+
+  #     end
+
+  # end
 
 
